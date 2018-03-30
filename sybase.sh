19edfc0fde82394f8bfe6cd77ae568b06f7cd86e
shekar_neo <shekarneo@users.noreply.github.com>
2018-03-30 15:30:48 +0530

    added pubs

--- sybase.sh
+++ sybase.sh
@@ -264,6 +264,41 @@ sleep 05
 exec crontab -e
 }
 
+#function pubs
+function pubs
+{
+export scpath="/opt/sybase/ASE-16_0/scripts"
+cd $scpath
+echo "------------------------------------------"
+echo "select a pub's database from below list "
+ls installpubs* --format single-column
+echo "------------------------------------------"
+read pubname
+if [ ! -f $pubname ]; then
+echo "********************************"
+echo "the $pubname not found try again"
+elif [ $pubname = "" ]; then
+echo "!!!!!!!!!!!!!!!!!!!!!!!!"
+echo "field cannot to be empty"
+else
+echo "$pubname found and enter required information"
+echo "##############################################"
+fi
+echo "enter server name"
+read uname
+echo "----------------------------------------------"
+echo "enter password"
+read pss
+echo 
+echo "server_name = $uname
+login_name = sa
+password = $pss"
+echo 
+xterm -hold -e "isql64 -Usa -S$uname -P$pss -i$pubname" 
+sleep 02
+return 0
+}
+
 #menu
 echo  
 echo "***************************"
@@ -288,6 +323,7 @@ case $x in
 2) backup_server ;;
 3) start_server ;;
 4) crontab ;;
+6) pubs ;;
 q) exit ;;
 * ) echo "enter "choice" or "q" "
 esac
